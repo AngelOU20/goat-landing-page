f18765cc63248c14b7c703686045836303a53e1a
Julio Ucharima <julio_ucharima@usmp.pe>
2024-03-27 16:31:52 -0500

    feat: add variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero": 'url(/hero.png)',
+        "about": 'url(/background-about.png)',
+      },
+      colors: {
+        primary: '#fafafa',
+        secondary: '#7c6a46'
       },
     },
   },
