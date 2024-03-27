8de01c9ac0e57885cc694743c36899049be4e98e
Julio Ucharima <julio_ucharima@usmp.pe>
2024-03-27 16:37:23 -0500

    feat: implement barrel file to images folder

---
+++ public/images/index.ts
@@ -0,0 +1,19 @@
+import iconSwim from './services/swim.svg';
+import iconWifi from './services/wifi.svg';
+import iconBreakfast from './services/breakfast.svg';
+import iconGym from './services/gym.svg';
+import iconGameCenter from './services/game.svg';
+import iconLight from './services/light.svg';
+import iconLaundry from './services/laundry.svg';
+import iconParking from './services/parking.svg';
+
+export {
+  iconSwim,
+  iconWifi,
+  iconBreakfast,
+  iconGym,
+  iconGameCenter,
+  iconLight,
+  iconLaundry,
+  iconParking,
+};
