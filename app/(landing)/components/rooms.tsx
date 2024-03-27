646fb58ac7fcddb38570f84fac9d78723681e374
Julio Ucharima <julio_ucharima@usmp.pe>
2024-03-27 16:36:35 -0500

    feat: add rooms component to page

---
+++ app/(landing)/components/rooms.tsx
@@ -0,0 +1,14 @@
+export const RoomTypes = () => {
+  return (
+    <section
+      className="md:max-w-screen-2xl mx-auto text-center scroll-mt-28"
+      id="rooms"
+    >
+      <div>
+        <h2 className="text-2xl md:text-3xl font-semibold mb-4">
+          Tipos de habitaciones
+        </h2>
+      </div>
+    </section>
+  );
+};
